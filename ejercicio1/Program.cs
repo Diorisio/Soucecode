c8d5ea95cfec8a0a35ade6fa7d8eb94466f35454
Diorisio <r578@hotmail.com>
2022-10-01 22:09:07 -0500

    Ejercicio1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe una frase");
+            String cadena = Console.ReadLine();
+            if (cadena.Length < 20)
+            {
+                for (int i=0;i<cadena.Length;i++) 
+                {
+                    Console.Write(cadena[i]);
+                }
+                    Console.Write("...");
+                
+            }
+            else
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    Console.Write(cadena[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
