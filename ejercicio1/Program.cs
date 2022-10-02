ea96003c6d6c2fb76de65f44a13d435b938f7edd
Diorisio <r578@hotmail.com>
2022-10-02 01:52:42 -0500

    ejercicio1

--- ejercicio1/Program.cs
+++ ejercicio1/Program.cs
@@ -21,6 +21,36 @@ namespace ejercicio1
             ejercicio2();
             //-------
         }
+        static void ejercicio3()
+        {
+            int p=0;
+            Console.WriteLine("Escribe cuantos numeron ingresaras: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("Ingrese los numeros");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Clear();
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            Console.WriteLine("Numeros organizados de mayor a menor");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]+ ",");
+            }
+            for(int i=0; i < arr.Length; i++)
+            {
+                if (arr[i]%2==0)
+                {
+                    p += 1;
+                }
+            }
+            Console.WriteLine("hay " + p + " numeros pares en la cadena");
+
+            Console.ReadKey();
+        }
 
         static void ejercicio2()
         {
